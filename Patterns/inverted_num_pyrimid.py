e90d3c119a73ef5c14100a2c143970a6b0182db4
Trijesh Kondapuram <trijeshkondapuram1@gmail.com>
2025-06-10 23:22:47 +0530

    patterns_day 1

---
+++ Patterns/inverted_num_pyrimid.py
@@ -0,0 +1,5 @@
+n=int(input())
+for i in range(n,0,-1):
+    for m in range(1,i+1):
+        print(m,end="")
+    print()
---
+++ Patterns/right_angled_num_pyramid_2.py
@@ -0,0 +1,7 @@
+#Right-Angled Number Pyramid 2
+
+n=int(input())
+for i in range(1,n+1):
+    for _ in range(i):
+        print(i,end="")
+    print()
