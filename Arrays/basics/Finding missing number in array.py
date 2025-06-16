82426a9640aeebdf276027130eb25af793685acd
Trijesh Kondapuram <trijeshkondapuram1@gmail.com>
2025-06-16 17:40:48 +0530

    Finding Missing values

---
+++ Arrays/basics/Finding missing number in array.py
@@ -0,0 +1,20 @@
+def missingNumber(a, N):
+    # Summation of first N numbers:
+    summation = (N * (N + 1)) // 2
+
+    # Summation of all array elements:
+    s2 = sum(a)
+
+    missingNum = summation - s2
+    return missingNum
+
+def main():
+    N = 5
+    a = [1, 2, 4, 5]
+    ans = missingNumber(a, N)
+    print("The missing number is:", ans)
+
+if __name__ == '__main__':
+    main()
+
+
