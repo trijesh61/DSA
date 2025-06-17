80c45b69dddd136ed6aa6fd35ff135a139a589a9
Trijesh Kondapuram <trijeshkondapuram1@gmail.com>
2025-06-17 14:52:48 +0530

    Longest Subarray

---
+++ Arrays/basics/Longest Subarray with given Sum K(Positives).py
@@ -0,0 +1,13 @@
+#Longest Subarray with given Sum K(Positives)
+
+a=list(map(int,input().split()))
+s=int(input())
+l=0
+ar=[]
+for i in range(len(a)-1):
+    for j in range(i+1,len(a)):
+        b=a[i:j]
+        if sum(b)==s and l<len(b):
+            l=len(b)
+            ar=b
+print(ar)
