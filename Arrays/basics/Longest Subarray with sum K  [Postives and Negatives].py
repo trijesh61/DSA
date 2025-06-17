c5d55ce6e88bccda75f9ec42c5da30c8519a5874
Trijesh Kondapuram <trijeshkondapuram1@gmail.com>
2025-06-17 14:54:42 +0530

    longest

--- Arrays/basics/Longest Subarray with sum K  [Postives and Negatives].py
+++ Arrays/basics/Longest Subarray with sum K  [Postives and Negatives].py
@@ -1,13 +1,39 @@
-#Longest Subarray with sum K | [Postives and Negatives]
 
-a=list(map(int,input().split()))
-s=int(input())
-l=0
-ar=[]
-for i in range(len(a)-1):
-    for j in range(i+1,len(a)):
-        b=a[i:j]
-        if sum(b)==s and l<len(b):
-            l=len(b)
-            ar=b
-print(ar)
+
+
+from typing import List
+
+def getLongestSubarray(a: [int], k: int) -> int:
+    n = len(a) # size of the array.
+
+    preSumMap = {}
+    Sum = 0
+    maxLen = 0
+    for i in range(n):
+        # calculate the prefix sum till index i:
+        Sum += a[i]
+
+        # if the sum = k, update the maxLen:
+        if Sum == k:
+            maxLen = max(maxLen, i + 1)
+
+        # calculate the sum of remaining part i.e. x-k:
+        rem = Sum - k
+
+        # Calculate the length and update maxLen:
+        if rem in preSumMap:
+            length = i - preSumMap[rem]
+            maxLen = max(maxLen, length)
+
+        # Finally, update the map checking the conditions:
+        if Sum not in preSumMap:
+            preSumMap[Sum] = i
+
+    return maxLen
+
+if __name__ == "__main__":
+    a = [-1, 1, 1]
+    k = 1
+    length = getLongestSubarray(a, k)
+    print(f"The length of the longest subarray is: {length}")
+
