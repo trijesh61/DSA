23bc6b5bc7456707da0b012c0a8707279d05766a
Trijesh Kondapuram <trijeshkondapuram1@gmail.com>
2025-06-18 21:52:20 +0530

    Move zeros

--- Arrays/basics/Move all Zeros to the end of the array.py
+++ Arrays/basics/Move all Zeros to the end of the array.py
@@ -1,8 +1,9 @@
 #Move all Zeros to the end of the array
 
 a=list(map(int,input().split()))
+pos=0
 for i in range(len(a)):
-    if a[i]==0:
-        a.pop(i)
-        a.append(0)
-print(a)
+    if a[i]!=0:
+        a[i],a[pos]=a[pos],a[i]
+        pos+=1
+    
