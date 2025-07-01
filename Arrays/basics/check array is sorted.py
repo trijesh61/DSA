05eb5d94f7e4c29d6754a1ef202bdfa9f50575a7
Trijesh Kondapuram <trijeshkondapuram1@gmail.com>
2025-07-01 19:50:50 +0530

    Update check array is sorted.py

--- Arrays/basics/check array is sorted.py
+++ Arrays/basics/check array is sorted.py
@@ -1,11 +1,11 @@
 #Problem Statement: Given an array of size n, write a program to check if the given array is sorted in (ascending / Increasing / Non-decreasing) order or not. If the array is sorted then return True, Else return False.
 
 def check(a):
-    l,t=len(a),True
+    l=len(a)
     for i in range(l-1):
         if a[i]>a[i+1]:
-            t=False
-    return t
+            return False
+    return True
 a=list(map(int,input().split()))
 if check(a):
     print("Array is Sorted!")
