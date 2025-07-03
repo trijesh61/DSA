e9e83d701c0f493bdf0cd97f22cf3b182b12640d
Trijesh Kondapuram <trijeshkondapuram1@gmail.com>
2025-07-03 22:24:34 +0530

    Update merge sort.py

--- Sorting/merge sort.py
+++ Sorting/merge sort.py
@@ -14,4 +14,14 @@ def merge_two_sorted_lists(left,right,arr):
     i=j=k=0
     while i<len(left) and j<len(right):
         if left[i]<=right[j]:
-            
+            arr[k]=left[i]
+            i+=1
+        else:
+            arr[k]=right[j]
+            j+=1
+        k+=1
+    while i<len(left):
+        arr[k]=left[i]
+        i+=1
+        k+=1
+    
