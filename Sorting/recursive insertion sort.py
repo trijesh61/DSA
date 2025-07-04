a854609cc959c706c2ca5af2741258f79600e911
Trijesh Kondapuram <trijeshkondapuram1@gmail.com>
2025-07-04 21:01:35 +0530

    Update recursive insertion sort.py

--- Sorting/recursive insertion sort.py
+++ Sorting/recursive insertion sort.py
@@ -1,4 +1,13 @@
 def recursive_insertion_sort(arr,n):
     if n <= 1:
         return
-    
+    recursive_insertion_sort(arr, n - 1)
+    last = arr[n - 1]
+    j = n - 2
+
+    while j >= 0 and arr[j] > last:
+        arr[j + 1] = arr[j]
+        j -= 1
+
+    arr[j + 1] = last
+
