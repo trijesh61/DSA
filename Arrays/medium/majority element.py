e2efc515f6b5cf8c8a1b80775bee2898f8e8053c
Trijesh Kondapuram <trijeshkondapuram1@gmail.com>
2025-07-13 21:52:51 +0530

    Create majority element.py

---
+++ Arrays/medium/majority element.py
@@ -0,0 +1,3 @@
+#Problem Statement: Given an array of N integers,
+# write a program to return an element that occurs more than N/2 times in the given array.
+# You may consider that such an element always exists in the array.
