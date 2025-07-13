d29aaa32d53d207af3100dd8d6e134443860b97b
Trijesh Kondapuram <trijeshkondapuram1@gmail.com>
2025-07-13 21:58:41 +0530

    Update majority element.py

--- Arrays/medium/majority element.py
+++ Arrays/medium/majority element.py
@@ -1,3 +1,13 @@
 #Problem Statement: Given an array of N integers,
 # write a program to return an element that occurs more than N/2 times in the given array.
 # You may consider that such an element always exists in the array.
+def find(n,arr):
+    t=n//2
+    for i in arr:
+        if arr.count(i)>=t:
+            return i
+
+n=int(input())
+arr=list(map(int,input().split()))
+t=find(n,arr)
+print(t)
