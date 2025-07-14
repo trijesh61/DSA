8d58277e4d090e1c6838e43e2dc92962cbb1fff1
Trijesh Kondapuram <trijeshkondapuram1@gmail.com>
2025-07-14 21:18:33 +0530

    Update Kadane's Algorithm  Maximum Subarray Sum in an Array.py

--- Arrays/medium/Kadane's Algorithm  Maximum Subarray Sum in an Array.py
+++ Arrays/medium/Kadane's Algorithm  Maximum Subarray Sum in an Array.py
@@ -1,1 +1,13 @@
-    
+#Kadane's Algorithm : Maximum Subarray Sum in an Array
+
+#Problem Statement: Given an integer array arr, 
+# find the contiguous subarray (containing at least one number) 
+# which has the largest sum and returns its sum and prints the subarray.
+
+arr=list(map(int,input().split()))
+m=0
+for i in arr:
+    for j in arr:
+        if (i+j)>max:
+            max=i+j
+print(max)
