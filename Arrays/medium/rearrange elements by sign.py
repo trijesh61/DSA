4192f05c34327770c0858ac7b5778e888cf03b02
Trijesh Kondapuram <trijeshkondapuram1@gmail.com>
2025-07-17 19:55:30 +0530

    Update rearrange elements by sign.py

--- Arrays/medium/rearrange elements by sign.py
+++ Arrays/medium/rearrange elements by sign.py
@@ -1,24 +1,26 @@
 #There’s an array ‘A’ of size ‘N’ with an equal number of positive and negative elements.
 #Without altering the relative order of positive and negative elements, you must return an array of alternately positive and negative values.
 
-l=[1,2,-4,-5]
 
-# 1 -4 2 -5
-n,p=[],[]
-for i in l:
-    if i>=0:
-        p.append(i)
-    else:
-        n.append(i)
-no=len(l)
+def rearranging_positives_negetives(l):
+    n,p=[],[]
+    for i in l:
+        if i>=0:
+            p.append(i)
+        else:
+            n.append(i)
+    no=len(l)
 
-neg,pos=0,0
-for i in range(no):
-    if i%2==0:
-        l[i]=p[pos]
-        pos+=1
-    else:
-        l[i]=n[neg]
-        neg+=1
+    neg,pos=0,0
+    for i in range(no):
+        if i%2==0:
+            l[i]=p[pos]
+            pos+=1
+        else:
+            l[i]=n[neg]
+            neg+=1 
 
+
+l=list(map(int,input("Enter Elements by Space seperated : ").split()))
+rearranging_positives_negetives(l)
 print(l)
