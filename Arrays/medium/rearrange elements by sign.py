37cf1871feebf1dff4727be6e5f698b394fcac48
Trijesh Kondapuram <trijeshkondapuram1@gmail.com>
2025-07-17 19:40:50 +0530

    Update rearrange elements by sign.py

--- Arrays/medium/rearrange elements by sign.py
+++ Arrays/medium/rearrange elements by sign.py
@@ -1,3 +1,13 @@
 #There’s an array ‘A’ of size ‘N’ with an equal number of positive and negative elements.
 #Without altering the relative order of positive and negative elements, you must return an array of alternately positive and negative values.
 
+l=[1,2,-4,-5]
+
+# 1 -4 2 -5
+n,p=[],[]
+for i in l:
+    if i>=0:
+        p.append(i)
+    else:
+        n.append(i)
+
