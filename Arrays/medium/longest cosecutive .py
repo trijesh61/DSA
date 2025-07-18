0c5a8942a82338f4e72f8db8fc5fd32100e6b358
Trijesh Kondapuram <trijeshkondapuram1@gmail.com>
2025-07-18 22:38:36 +0530

    Update longest cosecutive .py

--- Arrays/medium/longest cosecutive .py
+++ Arrays/medium/longest cosecutive .py
@@ -1,18 +1,26 @@
 #Problem Statement: You are given an array of ‘N’ integers.
 #  You need to find the length of the longest sequence which contains the consecutive elements.
 
-def longet_consecutive_numbers_length(arr):
-    nums=set(arr)
-    n=len(arr)
-    longest=0
+def longest_consecutive_numbers_length(arr):
+    nums = set(arr)
+    longest = 0
+
     for num in nums:
-        if num-1 not in nums:
-            length=1
-            while num+1 in nums:
-                length+=1
-            longest=max(longest,length)
+        # Check if this is the start of a sequence
+        if num - 1 not in nums:
+            length = 1
+            current_num = num
+            
+            # Count consecutive numbers after current_num
+            while current_num + 1 in nums:
+                current_num += 1    # increment to next number
+                length += 1
+            
+            longest = max(longest, length)
+    
     return longest
 
-arr=list(map(int,input().split()))
-l=longet_consecutive_numbers_length(arr)
+
+arr = list(map(int, input().split()))
+l = longest_consecutive_numbers_length(arr)
 print(l)
