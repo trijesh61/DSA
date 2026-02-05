26e697dd8af58c71b7d3781932ce0a578e5e43e1
Trijesh Kondapuram <trijeshkondapuram1@gmail.com>
2026-02-05 21:13:49 +0530

    Practice

--- Recursion/print 4 times.py
+++ Recursion/print 4 times.py
@@ -6,3 +6,14 @@ def printTrijesh(c):
     c-=1
     printTrijesh(c)
 printTrijesh(4)
+
+
+Count=0
+def print__():
+    global Count
+    if Count==4:
+        return
+    Count+=1
+    print("Trijeshhh.....")
+    print__()
+print__()
